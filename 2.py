fb0461e55b40f1ba1cbd7abfe8aeb3294b1d528e
orm-pokipsi <ilnur-gimazov@mail.ru>
2024-08-05 14:26:37 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,4 +2,4 @@ course_name = "Python"
 total_tasks = 12
 hours_spent = 1.5
 average_completion_time = hours_spent / total_tasks
-print('Курс:',course_name,', Всего задач',total_tasks,', Затрачено часов',hours_spent,', Среднее время выполнения',average_completion_time,)
+print('Курс:',course_name,', Всего задач',total_tasks,', Затрачено часов',hours_spent,', Среднее время выполнения',average_completion_time,'часа')
