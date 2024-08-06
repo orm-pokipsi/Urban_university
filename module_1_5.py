f8f099518820223d49740be01a56468b7534bc07
orm-pokipsi <ilnur-gimazov@mail.ru>
2024-08-06 16:36:54 +0300

    Add files via upload
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 'oppo','xiaomi','apple','samsung'
+print("Immutable tuple:",(immutable_var))
+#immutable_var [0] = "realmi" #если мы захотим изменить этот элемент, попросим вывести кортеж на экран, то столкнемся с ошибкой
+mutable_list = ['oppo','xiaomi','apple','samsung']
+mutable_list.extend(['realmi'])
+print("Mutable list:",(mutable_list))
