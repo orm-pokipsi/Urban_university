70c7fb588c1741d3336d5e73f393873536fc7534
orm-pokipsi <ilnur-gimazov@mail.ru>
2024-08-11 12:00:10 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,9 @@
 first = int(input('Введите число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
-if first / second == 1 and first / third == 1 and second / third == 1:
+if first == second  and first == third and second == third:
     print('3')# если совпали 3 введенных числа
-elif first / second == 1 or first / third == 1 or second / third == 1:
+elif first == second or first == third or second == third:
     print('2')# если совпали 2 введенных числа
 else:
     print('0')# если ни одно число не совпало
