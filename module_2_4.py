fdc390a2d88c92bfd5d45f9c838424a34f21819e
orm-pokipsi <ilnur-gimazov@mail.ru>
2024-08-12 19:29:22 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -3,8 +3,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 is_prime = True
-for i in range(len(numbers)):
-    if numbers[i] != 1:
+for i in range(1, len(numbers)):
         is_prime = True
         for j in range(2, numbers[i] - 1):
             if numbers[i] % j == 0:
