beca5381bf724a317ab10b9a33f3d890053e37dc
orm-pokipsi <ilnur-gimazov@mail.ru>
2024-09-28 20:28:20 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+print('Задание "Задача "А как делить?":')
+from fake_math import divide as fm
+from true_math import divide as tm
+print('result1 = ', fm(69, 3))
+print('result2 = ', fm(3, 0))
+print('result3 = ', tm(49, 7))
+print('result4 = ', tm(15, 0))
