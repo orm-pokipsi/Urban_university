4eca8052a33f7244f42ee7f22240e6736ed66412
Ilnur Gimazov <ilnur-gimazov@mail.ru>
2024-11-22 17:07:59 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -7,25 +7,32 @@ def read_info(name):
     with open(name, 'r', encoding='utf8') as file:
         while True:
             line = file.readline().strip()
-            all_data.append(line)
             if not line:
                 break
+            all_data.append(line)
+    return all_data
 
-
-filenames = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
-start1 = time.time()
-
-# for i in filenames:
-#     read_info(i)
-#     end1 = time.time()
-#     time_of_line_function = end1 - start1
-#     print(f'Линейный вызов : {time_of_line_function}')
 
 if __name__ == '__main__':
+    filenames = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
+
+    total_time = 0
+
+    for i in filenames:
+        start1 = time.time()
+        read_info(i)
+        end1 = time.time()
+
+        time_of_data_function = end1 - start1
+        total_time += time_of_data_function
+
+    print(f"Линейный вызов : {total_time:}")
+
     start_time_parallel = time.time()
 
     with multiprocessing.Pool(processes=4) as pool:
         pool.map(read_info, filenames)
+
     end_time_parallel = time.time()
     time_of_multiprocessing = end_time_parallel - start_time_parallel
-    print(f"Многопроцессный : {time_of_multiprocessing:.2f} секунд")
+    print(f"Многопроцессный: {time_of_multiprocessing:}")
